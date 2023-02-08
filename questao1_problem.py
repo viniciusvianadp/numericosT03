30294d2e798e8429c2785638ad72b94ccb06596b
Vinícius Viana <viniciusviana020@gmail.com>
2023-02-08 13:36:00 -0300

    Problema com solução exata desconhecida (questão 1)

---
+++ questao1_problem.py
@@ -0,0 +1,102 @@
+## 2023.02.08
+## Keith Ando Ogawa - keith.ando@usp.br
+## Vinícius Viana de Paula - viniciusviana@usp.br
+
+# MAP5725
+
+# using Trapezoidal method and SAM to find the solution to a bidimensional problem.
+
+# problem with unkwnown exact solution 
+#              (1) x'= 1.2x - x² - xy/(x + 0.2)     0<=t<=5
+#              (2) y'= 1.5xy/(x + 0.2) - y          x(0) = 1; y(0) = 1
+                         
+
+import math
+import numpy as np
+
+#############################################################################
+
+def phi(t1, y1, t2, y2, f):
+    # define discretization function 
+    return 0.5*(f(t1, y1)+f(t2, y2))     # euler 
+
+############################################################################
+
+def f(t, y):
+    # bidimensional problem
+    f0 =  1.2*y[0] - (y[0])**2 - y[0]*y[1]/(y[0] + 0.2)
+    f1 =  1.5*y[0]*y[1]/(y[0] + 0.2) - y[1]
+    
+    return np.array([f0, f1])
+
+############################################################################
+
+def implicitMethod(t0, y0, T, n):
+    # compute approximate solution to the initial value problem
+
+    y = [np.array(y0)]
+    t = [t0]
+
+    h = (T - t0) / n
+
+    while t[-1] < T:
+        # initial guess
+        if(np.array(t).size > 1): 
+            ytil = y[-1] + h*phi(t[-1], y[-1], t[-2], y[-2], f)
+        else:
+            ytil = y[-1] + h*phi(t[-1], y[-1], t[-1], y[-1], f)
+        diff = 1.0
+
+        # fixed point iteration
+        r = 0
+        while r<20 and diff > 0.0001:
+            ytil0 = ytil
+            ytil = y[-1] + h*phi(t[-1], y[-1], t[-1] + h, ytil, f)
+            diff = np.linalg.norm(ytil - ytil0)
+            r = r + 1
+        y.append(ytil) # y(i+1) = ytil
+        t.append(t[-1] + h)
+    y = np.array(y)
+    
+    return (T - t0) / n, y[-1]
+
+############################################################################
+
+def main():
+    # obtains the numerical convergence table based on parameters such as
+    # inicial conditions, final time and number of steps
+
+    # input numerical model data
+    t0=0; y0=np.array([1, 1]);  # initial condition
+    T=5             # final time
+    
+    # input numerical method data
+    m=13;  h=[0]*m;   # number of cases to run. Initialize list of time steps
+    yn=[y0]*m;       # initialize list of approximations
+    
+    with open("behavior_convergence.txt", 'w', encoding='utf-8') as file2:
+        file2.write("ORDER BEHAVIOR CONVERGENCE TABLE\n");
+    
+        e=p=q=r=s1=s2=0;
+        for i in range(1,m+1):
+            n=16*2**(i-1); 
+
+            h[i-1],yn[i-1]=implicitMethod(t0,y0,T,n);
+            if i>2:
+                z3= np.array(yn[i-3])
+                z2 = np.array(yn[i-2])
+                z1 = np.array(yn[i-1])
+                s1 = np.sqrt(((z3 - z2)[0])**2 +((z3 - z2)[1])**2)
+                s2 = np.sqrt(((z2 - z1)[0])**2 +((z2 - z1)[1])**2)
+
+                q = s1/s2;
+                r = h[i-2]/h[i-1];
+            
+                p = math.log(q)/math.log(r);
+            
+                e = (z2-z1)[0]**2 + (z2-z1)[1]**2; 
+                file2.write("{:5d} & {:9.3e} & {:9.3e} & {:9.3e}\\\\\n".format(n,h[i-1],e,p)) 
+
+############################################################################
+         
+main()
--- questao1_problem_plot.py
+++ questao1_problem_plot.py
@@ -6,9 +6,10 @@
 
 # plots for general implicit one-Step methods.
 
-# (manufactured) problem with kwnown exact solution 
-#              (1) y_1'= y_2              1<=t<=5
-#              (2) y_2'= -(1/t)y_2        y_1(1) = 2; y_2(1) = 1
+# problem with unkwnown exact solution 
+#              (1) x'= 1.2x - x² - xy/(x + 0.2)     0<=t<=5
+#              (2) y'= 1.5xy/(x + 0.2) - y          x(0) = 1; y(0) = 1
+
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -23,8 +24,8 @@ def phi(t1, y1, t2, y2, f):
 
 def f(t, y):
    # bidimensional problem
-    f0 =  y[1]
-    f1 =  -(1/t)*y[1]
+    f0 =  1.2*y[0] - (y[0])**2 - y[0]*y[1]/(y[0] + 0.2)
+    f1 =  1.5*y[0]*y[1]/(y[0] + 0.2) - y[1]
     
     return np.array([f0, f1])
 
@@ -59,9 +60,9 @@ def implicitMethod(T, n, yn, tn, f):
 ############################################################################
 
 # other relevant data
-t_n_1 = [1]; t_n_2 = [1]; t_n_3 = [1]; T = 5;        # time interval: t in [t0,T]
-y_n_1 = [np.array([2, 1])]; y_n_2 = [np.array([2, 1])];
-y_n_3 = [np.array([2, 1])]; # initial condition
+t_n_1 = [0]; t_n_2 = [0]; t_n_3 = [0]; T = 5;        # time interval: t in [t0,T]
+y_n_1 = [np.array([1, 1])]; y_n_2 = [np.array([1, 1])];
+y_n_3 = [np.array([1, 1])]; # initial condition
 
 n_1 = 16                # time interval partition (discretization)
 y_n_1, t_n_1 = implicitMethod(T, n_1, y_n_1, t_n_1, f)
@@ -96,24 +97,3 @@ plt.title('Aproximação Numérica da Variável de Estado y2')
 plt.legend()
 plt.show()
 
-## exact vs approximated (y1)
-t = np.linspace(1, 5, 65536)
-plt.plot(t, np.log(t) + 2, 'k-', label = 'solução exata')
-plt.plot(t_n_3, y_n_3[:, 0], 'k--', label = 'solução numérica')
-
-plt.xlabel('t  (em unidade de tempo)')
-plt.ylabel('y1(t)  (em unidade de y1)')
-plt.title('Soluções Aproximada e Exata Para a Variável y1')
-plt.legend()
-plt.show()
-
-## exact vs approximated (y2)
-t = np.linspace(1, 5, 65536)
-plt.plot(t, (1/t), 'k-', label = 'solução exata')
-plt.plot(t_n_3, y_n_3[:, 1], 'k--', label = 'solução numérica')
-
-plt.xlabel('t  (em unidade de tempo)')
-plt.ylabel('y2(t)  (em unidade de y2)')
-plt.title('Soluções Aproximada e Exata Para a Variável y2')
-plt.legend()
-plt.show()
